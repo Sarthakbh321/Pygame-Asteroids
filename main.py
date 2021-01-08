53c10111ea78ffe4a9ce615fa63dd5761f8713d4
Sarthak <sarthakbh321@gmail.com>
2021-01-09 01:57:21 +0530

    draw spaceship images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 
@@ -9,9 +10,17 @@ WHITE = (255, 255, 255)
 
 FPS = 60
 
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 45
+
+RIGHT_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_red.png"))
+RIGHT_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RIGHT_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_WIDTH)), -90)
+LEFT_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
+LEFT_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(LEFT_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
 def draw_graphics():
     WIN.fill(WHITE)
-
+    WIN.blit(LEFT_SPACESHIP, (300, 100))
+    WIN.blit(RIGHT_SPACESHIP, (350, 100))
     pygame.display.update()
 
 def main():
