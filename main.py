12d72dae2405db2be79fcdd1bb1b491dc0fce97e
Sarthak <sarthakbh321@gmail.com>
2021-01-09 02:08:46 +0530

    added input for left spaceship

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("Asteroids vs.")
 WHITE = (255, 255, 255)
 
 FPS = 60
+VELOCITY = 5
 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 45
 
@@ -17,17 +18,33 @@ RIGHT_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RIGHT_SPACESHIP
 LEFT_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
 LEFT_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(LEFT_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
-def draw_graphics():
+def draw_graphics(left, right):
     WIN.fill(WHITE)
-    WIN.blit(LEFT_SPACESHIP, (300, 100))
-    WIN.blit(RIGHT_SPACESHIP, (350, 100))
+    WIN.blit(LEFT_SPACESHIP, (left.x, left.y))
+    WIN.blit(RIGHT_SPACESHIP, (right.x, right.y))
     pygame.display.update()
+
+def handle_input(left, right):
+    keys_pressed = pygame.key.get_pressed()
+
+    if(keys_pressed[pygame.K_w]):
+        left.y -= VELOCITY
+    if(keys_pressed[pygame.K_d]):
+        left.x += VELOCITY
+    if (keys_pressed[pygame.K_s]):
+        left.y += VELOCITY
+    if (keys_pressed[pygame.K_a]):
+        left.x -= VELOCITY
+
+
 
 def main():
     clock = pygame.time.Clock()
 
-    run = True
+    right = pygame.Rect(700, 250, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    left = pygame.Rect(100, 250, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
+    run = True
     while(run):
 
         clock.tick(FPS)
@@ -35,7 +52,9 @@ def main():
             if(event.type == pygame.QUIT):
                 run = False
 
-        draw_graphics()
+
+        handle_input(left, right)
+        draw_graphics(left, right)
 
     pygame.quit()
 
