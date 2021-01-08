f083a183eb2b3abf80616a2fa623457d56277e23
Sarthak <sarthakbh321@gmail.com>
2021-01-09 01:41:07 +0530

    added FPS

--- main.py
+++ main.py
@@ -3,16 +3,30 @@ import pygame
 WIDTH, HEIGHT = 900, 500
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Asteroids vs.")
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+
+def draw_graphics():
+    WIN.fill(WHITE)
+
+    pygame.display.update()
 
 def main():
+    clock = pygame.time.Clock()
+
     run = True
 
     while(run):
 
+        clock.tick(FPS)
         for event in pygame.event.get():
             if(event.type == pygame.QUIT):
                 run = False
 
+        draw_graphics()
 
     pygame.quit()
 
