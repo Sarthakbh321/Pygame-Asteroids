3e11d9e1a71c7cf35527da70c2d4cc251c8b2442
Sarthak <sarthakbh321@gmail.com>
2021-01-09 12:54:10 +0530

    started bullets logic

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ RIGHT_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RIGHT_SPACESHIP
 LEFT_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
 LEFT_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(LEFT_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
+BULLET_VELOCITY = 7
+MAX_BULLETS = 3
+
+
 def draw_graphics(left, right):
     WIN.fill(WHITE)
     pygame.draw.rect(WIN, BLACK, BORDER)
@@ -55,6 +59,10 @@ def main():
     right = pygame.Rect(700, 250, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     left = pygame.Rect(100, 250, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
+    left_bullets = []
+    right_bullets = []
+
+
     run = True
     while(run):
 
@@ -63,8 +71,18 @@ def main():
             if(event.type == pygame.QUIT):
                 run = False
 
+            if(event.type == pygame.KEYDOWN):
+
+                if(event.key == pygame.K_LSUPER and len(left_bullets) < MAX_BULLETS):
+                    bullet = pygame.Rect(left.x + left.width, left.y + left.height/2 - 2, 10, 5)
+                    left_bullets.append(bullet)
+
+                if(event.key == pygame.K_RSUPER and len(right_bullets) < MAX_BULLETS):
+                    bullet = pygame.Rect(right.x, right.y + right.height / 2 - 2, 10, 5)
+                    right_bullets.append(bullet)
 
 
+        # print(right_bullets, left_bullets)
         keys_pressed = pygame.key.get_pressed()
         handle_input_left(keys_pressed, left)
         handle_input_right(keys_pressed, right)
